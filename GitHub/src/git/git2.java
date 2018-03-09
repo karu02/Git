e61ee26b3ad8440daf87875473d067c35cfee1c3
karu02 <37206208+karu02@users.noreply.github.com>
2018-03-09 13:36:10 +0530

    Update git2.java

--- GitHub/src/git/git2.java
+++ GitHub/src/git/git2.java
@@ -5,7 +5,7 @@ public class git2
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Github Repository Created");
+		System.out.println("Github Repository Created Hello How are u Github");
 		
 
 	}

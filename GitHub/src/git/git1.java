83181e0f87278f67eeb5e9acd7073d4aa870f9ef
hpr063 <hpr063@hp>
2018-03-09 13:20:24 +0530

    Hello

--- GitHub/src/git/git1.java
+++ GitHub/src/git/git1.java
@@ -2,8 +2,9 @@ package git;
 
 public class git1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Karthik How do you do");
 
 	}
 

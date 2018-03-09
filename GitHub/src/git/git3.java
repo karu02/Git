94e00b75dc4a05d1e64830947366aa9bff9e5a5a
hpr063 <hpr063@hp>
2018-03-09 14:02:20 +0530

    karu

--- GitHub/src/git/git3.java
+++ GitHub/src/git/git3.java
@@ -1,9 +1,11 @@
 package git;
 
-public class git3 {
+public class git3 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("Github is super");
 
 	}
 
